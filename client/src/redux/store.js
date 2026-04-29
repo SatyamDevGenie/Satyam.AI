2375064f7c9901aeab969160c078f631d4a67d63
SatyamDevGenie <satyamsawant54@gmail.com>
2026-04-30 04:44:43 +0530

    userData visible in redux-devtools extension

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import userSlice from './userSlice'
+
 export default configureStore({
-    reducer: {}
+    reducer: {
+        user: userSlice
+    }
 })
