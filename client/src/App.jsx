b1ac42f9cb3d605bd26f6a7d17d6328c8c6eff35
SatyamDevGenie <satyamsawant54@gmail.com>
2026-04-29 13:26:16 +0530

    Designed a Auth Page with a following icons with installation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Auth from './pages/Auth'
 
 function App() {
   return (
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/auth" element={<Auth />} />
+    </Routes>
   )
 }
 
