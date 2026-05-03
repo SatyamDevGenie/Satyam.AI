be0cd7160fb57e9377b110a2fc269e6286ae0f31
SatyamDevGenie <satyamsawant54@gmail.com>
2026-05-03 10:59:55 +0530

    Logic for startMic(), stopMic() --- Step2Interview.jsx Page

--- client/src/components/Step2Interview.jsx
+++ client/src/components/Step2Interview.jsx
@@ -213,6 +213,29 @@ function Step2Interview({ interviewData, onFinish }) {
   }, []);
 
 
+  const startMic = () => {
+    if (recognitionRef.current && !isAIPlaying) {
+      try {
+        recognitionRef.current.start();
+      } catch { }
+    }
+  };
+
+  const stopMic = () => {
+    if (recognitionRef.current) {
+      recognitionRef.current.stop();
+    }
+  };
+  const toggleMic = () => {
+    if (isMicOn) {
+      stopMic();
+    } else {
+      startMic();
+    }
+    setIsMicOn(!isMicOn);
+  };
+
+
 
   return (
     <div className='min-h-screen bg-linear-to-br from-emerald-50 via-white to-teal-100 flex items-center justify-center p-4 sm:p-6'>
